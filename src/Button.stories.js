bd52084d25d3e02348a0520cb4d63ee013465567
Myungjin Kim <weumj1@gmail.com>
2018-10-02 15:35:25 +0900

    feat: with knobs

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-import { storiesOf, setAddon } from "@storybook/react";
-import JSXAddon from "storybook-addon-jsx";
+import { storiesOf, setAddon, addDecorator } from "@storybook/react";
 
+import JSXAddon from "storybook-addon-jsx";
 setAddon(JSXAddon);
+
+import { withKnobs } from "@storybook/addon-knobs/react";
+addDecorator(withKnobs);
 
 import { Button } from "./Button";
 import { wInfo } from "./utils";
+
+import { color } from "@storybook/addon-knobs/react";
 
 storiesOf("Button", module)
   .addWithJSX(
@@ -20,5 +25,5 @@ storiesOf("Button", module)
   `)(() => <Button bg="palegoldenrod">Hello world</Button>)
   )
   .addWithJSX("with background 2", () => (
-    <Button bg="green">Hello world2</Button>
+    <Button bg={color("bg", "green", "group1")}>Hello world2</Button>
   ));
